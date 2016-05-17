d58f216ba5558aaf55d22fc808e2ca969e7ee24a
Wojciech Bednarski <wbednarski@gmail.com>
2016-05-17 15:20:06 +0200

    Tests - Registered User

--- test/controllers/registered_users_controller_test.rb
+++ test/controllers/registered_users_controller_test.rb
@@ -5,30 +5,48 @@ class RegisteredUsersControllerTest < ActionDispatch::IntegrationTest
     @registered_user = registered_users(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get registered_users_url
     assert_response :success
   end
 
-  test "should create registered_user" do
+  test 'should create registered_user' do
     assert_difference('RegisteredUser.count') do
-      post registered_users_url, params: { registered_user: { date: @registered_user.date, number: @registered_user.number } }
+      post registered_users_url, params: { data: { type: 'registered-users', attributes: {date: @registered_user.date, number: @registered_user.number}}}
     end
 
     assert_response 201
   end
 
-  test "should show registered_user" do
+  test 'create registered_user should return correct value in JSON' do
+    assert_difference('RegisteredUser.count') do
+      post registered_users_url, params: { data: { type: 'registered-users', attributes: {date: @registered_user.date, number: @registered_user.number}}}
+    end
+
+    assert_equal JSON.parse(response.body)['data']['attributes']['number'], @registered_user.number
+  end
+
+  test 'should show registered_user' do
     get registered_user_url(@registered_user)
     assert_response :success
   end
 
-  test "should update registered_user" do
-    patch registered_user_url(@registered_user), params: { registered_user: { date: @registered_user.date, number: @registered_user.number } }
+  test 'should eturn correct JSON structure for registered_user' do
+    get registered_user_url(@registered_user)
+    assert_equal JSON.parse(response.body)['data']['attributes']['number'], @registered_user.number
+  end
+
+  test 'should update registered_user' do
+    patch registered_user_url(@registered_user), params: { data: { type: 'registered-users', attributes: {date: @registered_user.date, number: @registered_user.number}}}
     assert_response 200
   end
 
-  test "should destroy registered_user" do
+  test 'update registered_user should return correct value in JSON' do
+    patch registered_user_url(@registered_user), params: { data: { type: 'registered-users', attributes: {date: @registered_user.date, number: @registered_user.number}}}
+    assert_equal JSON.parse(response.body)['data']['attributes']['number'], @registered_user.number
+  end
+
+  test 'should destroy registered_user' do
     assert_difference('RegisteredUser.count', -1) do
       delete registered_user_url(@registered_user)
     end
@@ -36,3 +54,4 @@ class RegisteredUsersControllerTest < ActionDispatch::IntegrationTest
     assert_response 204
   end
 end
+
--- test/models/registered_user_test.rb
+++ test/models/registered_user_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class RegisteredUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Registered User exists' do
+    result = RegisteredUser.new
+    assert result
+  end
+
+  test 'Registered User has a number' do
+    result = RegisteredUser.new(number: 37)
+    assert_equal 37, result.number
+  end
+
+  test 'Registered User has a date' do
+    result = RegisteredUser.new(date: 37)
+    assert_equal 37, result.date
+  end
+
+  test 'fails when required date field is not present for Registered User' do
+    result = RegisteredUser.new(number: 37)
+    refute result.valid?
+  end
+
+  test 'creates Registered User when given valid attributes' do
+    result = RegisteredUser.new(date: Time.now, number: 37)
+    assert result.valid?
+  end
 end
