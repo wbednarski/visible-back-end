fbd2532f0bff6ef4600a4d75b565ab654efdfc82
Wojciech Bednarski <wbednarski@gmail.com>
2016-05-17 19:21:09 +0200

    Kebabize Registered User path
    TODO find out how kebabize all resources at once, few options there nothing really good.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :registered_users
+  resources :registered_users, path: 'registered-users' # TODO find out how kebabize all resources at once, few options there nothing really good
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
